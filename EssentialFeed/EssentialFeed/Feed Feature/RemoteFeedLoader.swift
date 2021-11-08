978f9d86d9ddae45bd40c703051e0df5a673d2b7
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2021-11-08 09:09:17 -0300

    Implement RemoteFeedLoader and RemoteFeedLoaderTests

--- EssentialFeed/EssentialFeed/Feed Feature/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/RemoteFeedLoader.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 final class RemoteFeedLoader {
-    
+    let client: HTTPClient
+    let url: URL
+
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
 }
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -5,8 +5,45 @@
 //  Created by Anne Kariny Silva Freitas on 06/11/21.
 //
 
+@testable import EssentialFeed
 import XCTest
 
 final class RemoteFeedLoaderTests: XCTestCase {
+    private let httpClientSpy = HTTPClientSpy()
 
+    func test_init_doesNotRequestDataFromURL() {
+        makeSut(url: URL(string: "test-url.com")!)
+        XCTAssertNil(httpClientSpy.getURLRequested)
+    }
+
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "test-url.com")!
+        let sut = makeSut(url: url)
+
+        sut.load()
+
+        XCTAssertTrue(httpClientSpy.getCalled)
+        XCTAssertNotNil(httpClientSpy.getURLRequested)
+        XCTAssertEqual(url, httpClientSpy.getURLRequested)
+    }
+}
+
+// MARK: - Helpers
+private extension RemoteFeedLoaderTests {
+    @discardableResult
+    private func makeSut(url: URL) -> RemoteFeedLoader {
+        RemoteFeedLoader(client: httpClientSpy, url: url)
+    }
+
+    // MARK: - Doubles/Spies/Mocks
+    final class HTTPClientSpy: HTTPClient {
+
+        private(set) var getCalled = false
+        var getURLRequested: URL?
+
+        func get(from url: URL) {
+            getCalled = true
+            getURLRequested = url
+        }
+    }
 }
