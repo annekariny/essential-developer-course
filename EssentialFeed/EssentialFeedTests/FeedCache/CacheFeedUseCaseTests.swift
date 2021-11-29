b498252a8377f23c47a0e9bfdb37f316a99419dc
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2021-11-29 09:27:21 -0300

    Checks if when initialize store, there's no save or delete being called

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -5,4 +5,43 @@
 //  Created by Anne on 29/11/21.
 //
 
-import Foundation
+import XCTest
+import EssentialFeed
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+}
+
+class FeedStore {
+    enum ReceivedMessage: Equatable {
+        case deletion
+        case insertion(items: [FeedItem])
+    }
+    
+    var receivedMessages = [ReceivedMessage]()
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+}
+
+private extension CacheFeedUseCaseTests {
+    func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (localFeedLoader: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let localFeedLoader = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(localFeedLoader, file: file, line: line)
+        return (localFeedLoader, store)
+    }
+}
