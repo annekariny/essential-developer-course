8bdd5bfbfee3d2382f388d3aad26c6dcd0e76a78
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2021-12-01 10:35:55 -0300

    Test save required delete cache feed deletion

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -14,15 +14,40 @@ class LocalFeedLoader {
     init(store: FeedStore) {
         self.store = store
     }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed { error in
+            if error == nil {
+                
+            } else {
+                
+            }
+        }
+    }
 }
 
 class FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
     enum ReceivedMessage: Equatable {
-        case deletion
-        case insertion(items: [FeedItem])
+        case deleteCachedFeed
+        case insert([FeedItem], Date)
     }
     
-    var receivedMessages = [ReceivedMessage]()
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCacheFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -31,9 +56,40 @@ class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [])
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let (localFeedLoader, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        localFeedLoader.save(items)
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed])
+    }
+    
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let (localFeedLoader, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        localFeedLoader.save(items)
+        store.completeDeletion(with: anyNSError)
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed])
+    }
 }
 
 private extension CacheFeedUseCaseTests {
+    var anyURL: URL {
+        URL(string: "http://any-url.com")!
+    }
+    
+    var anyNSError: NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
+    func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL)
+    }
+    
     func makeSUT(
         file: StaticString = #filePath,
         line: UInt = #line
