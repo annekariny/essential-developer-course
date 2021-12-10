9c73d1f062129c2b318fcfbe0fa5897bb258cd6a
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2021-12-10 08:12:56 -0300

    Create tests from LoadFeedFromCache use case

--- EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -5,4 +5,73 @@
 //  Created by Anne on 10/12/21.
 //
 
-import Foundation
+import EssentialFeed
+import XCTest
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let error = anyNSError
+        
+        expect(sut, toCompleteWith: .failure(error)) {
+            store.completeRetrieval(with: error)
+        }
+    }
+}
+
+private extension LoadFeedFromCacheUseCaseTests {
+    var anyNSError: NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
+    func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (
+        localFeedLoader: LocalFeedLoader,
+        store: FeedStoreSpy
+    ) {
+        let store = FeedStoreSpy()
+        let localFeedLoader = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(localFeedLoader, file: file, line: line)
+        return (localFeedLoader, store)
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedImages), .success(expectedImages)):
+                XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+                
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
+}
