6ea891417814e07aa2e7acfe840592d91c2b8d6f
annekarinyf <karinyfreitas.1994@gmail.com>
2022-10-31 21:51:10 -0300

    - Render all loaded feed items (location, image, description) - Image loading experience - Load when image view is visible (on screen) - Cancel when image view is out of screen - Show a loading indicator while loading image (shimmer) - Option to retry on image download error - Preload when image view is near visible - Cancel preload when image view is not near visible anymore

--- EssentialFeed/EssentialFeediOS/FeedImageCell.swift
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -5,4 +5,24 @@
 //  Created by Anne on 31/10/22.
 //
 
-import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+    
+    private(set) public lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    var onRetry: (() -> Void)?
+    
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
+}
