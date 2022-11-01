e6afefd756035edec0a6da4327ff0de4f695e5b2
annekarinyf <karinyfreitas.1994@gmail.com>
2022-10-31 21:49:59 -0300

    Create FeedViewController and tests

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Anne on 31/10/22.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeediOS/UIView+Shimmering.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Shimmering.swift
+//  EssentialFeediOS
+//
+//  Created by Anne on 31/10/22.
+//
+
+import Foundation
