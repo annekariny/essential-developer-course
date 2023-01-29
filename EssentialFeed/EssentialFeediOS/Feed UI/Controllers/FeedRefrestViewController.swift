89b639ca0e7ac8e1485ee1e969b1ed4acc988dbe
Anne Kariny Silva Freitas <annekarinyfreitas@gmail.com>
2023-01-29 11:52:24 -0300

    Change FeedRefreshViewModel to be Stateless, create generic Observer to help with state changes

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefrestViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefrestViewController.swift
@@ -24,11 +24,11 @@ final class FeedRefreshViewController: NSObject {
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         
         // Bind the viewModel with the view
-        viewModel.onChange = { [weak self] viewModel in
-            if viewModel.isLoading {
-                self?.view.beginRefreshing()
+        viewModel.onLoadingStateChange = { [weak view] isLoading in
+            if isLoading {
+                view?.beginRefreshing()
             } else {
-                self?.view.endRefreshing()
+                view?.endRefreshing()
             }
         }
         return view
--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedRefreshViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedRefreshViewModel.swift
@@ -9,28 +9,24 @@ import Foundation
 import EssentialFeed
 
 final class FeedRefreshViewModel {
+    typealias Observer<T> = (T) -> Void
+    
     private let feedLoader: FeedLoader
-    
-    var onChange: ((FeedRefreshViewModel) -> Void)?
-    var onFeedLoad: (([FeedImage]) -> Void)?
-    
-    private(set) var isLoading: Bool = false {
-        didSet {
-            onChange?(self)
-        }
-    }
 
     init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
     }
     
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
+    
     func loadFeed() {
-        isLoading = true
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get() {
                 self?.onFeedLoad?(feed)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
